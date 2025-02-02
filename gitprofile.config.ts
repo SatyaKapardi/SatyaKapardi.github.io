c6256eee88220c1a4fa6a89e3a533047023ea1fe
Srisatya Kapardi Budi <94280881+SatyaKapardi@users.noreply.github.com>
2025-02-02 22:46:45 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -66,7 +66,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1_ET8MTQ55NUy2FwOEn9LLzR_XgNusIUs/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
